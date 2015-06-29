2372b806318afa83321596560068d9068e63cc47
ivyduk <ivanduquecs@gmail.com>
2015-06-29 17:24:50 -0500

    formula urqt added again

---
+++ urqt.rb
@@ -0,0 +1,13 @@
+class urqt < Formula
+  homepage "https://github.com/l-modolo/UrQt"
+  head "https://github.com/l-modolo/UrQt.git", :using => :git
+ 
+
+
+  def install
+    system "cd UrQt"	
+    system "make"
+    bin.install "UrQt"
+    doc.install "README.md", "DOCUMENTATION"	
+  end  	
+end
